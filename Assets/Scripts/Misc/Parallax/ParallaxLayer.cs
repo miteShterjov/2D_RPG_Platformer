20fd6733f6ea5ba62519ead515b284ffd63c6cf9
jummbalaya <mite.shterjov@gmail.com>
2025-10-06 15:28:31 +0200

    Add skeleton sprite sheets and parallax background functionality
    - Added Skeleton React sprite sheet and its metadata.
    - Added Skeleton Walk sprite sheet and its metadata.
    - Created ParallaxBackground script to manage parallax scrolling of background layers.
    - Implemented ParallaxLayer class to handle individual layer movement and looping.
    - Added metadata files for new scripts and assets.

---
+++ Assets/Scripts/Misc/Parallax/ParallaxLayer.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ParallaxLayer
+{
+    [SerializeField] private Transform background;
+    [SerializeField] private float parallaxMultiplier;
+    [SerializeField] private float imageWidthOffset = 10;
+
+    private float imageFullWidth;
+    private float imageHalfWidth;
+
+    public void CalculateImageWidth()
+    {
+        SpriteRenderer spriteRenderer = background.GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = background.GetComponentInChildren<SpriteRenderer>();
+            if (spriteRenderer == null)
+            {
+                Debug.LogError("No SpriteRenderer found on the background object or its children.");
+                return;
+            }
+        }
+
+        imageFullWidth = spriteRenderer.bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2;
+    }
+
+    public void Move(float distanceToMove)
+    {
+        background.position += Vector3.right * (distanceToMove * parallaxMultiplier);
+    }
+
+    public void LoopBackground(float cameraLefteEdge, float cameraRightEdge)
+    {
+        float imageRightEdge = (background.position.x + imageHalfWidth) - imageWidthOffset;
+        float imageLeftEdge = (background.position.x - imageHalfWidth) + imageWidthOffset;
+
+        if (imageRightEdge < cameraLefteEdge)
+            background.position += Vector3.right * imageFullWidth;
+        else if (imageLeftEdge > cameraRightEdge)
+            background.position += Vector3.right * -imageFullWidth;
+    }
+
+}
